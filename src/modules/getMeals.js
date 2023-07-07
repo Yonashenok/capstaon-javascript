f334584465f7e53787cc3437f842b0038582498a
Yonas Henok <99332080+Yonashenok@users.noreply.github.com>
2023-07-07 20:33:03 +0300

    Update getMeals.js

--- src/modules/getMeals.js
+++ src/modules/getMeals.js
@@ -6,22 +6,22 @@ const getMeals = async () => {
   const { meals } = await fetchPro(
     'https://www.themealdb.com/api/json/v1/1/filter.php?c=vegetarian'
   );
-  const markUp = meals
-    ?.map((show) => {
-      return `
-        <div class="flex  flex-col flex-center meal-card">
-            <div><img class="meal-img" src="" alt="" /></div>
-              <p class="space">${show.strMeal}</p>
-                <div class="like flex flex-center">
-                <button class="like-btn" data-show= "${show.idMeal}" ><i class="fa-sharp fa-regular fa-heart fa-xl"></i></button>
-                <div class="flex"><p class="like-${show.idMeal}" >5</p><p>like</p></div>
-                </div>
-            <button data-tap= "${show.idMeal}" class="btn btn-comment">Comment</button>
-        </div>
-        `;
-    })
-    .join('');
-  mealContainer.insertAdjacentHTML('afterbegin', markUp);
+  // const markUp = meals
+  //   ?.map((show) => {
+  //     return `
+  //       <div class="flex  flex-col flex-center meal-card">
+  //           <div><img class="meal-img" src="" alt="" /></div>
+  //             <p class="space">${show.strMeal}</p>
+  //               <div class="like flex flex-center">
+  //               <button class="like-btn" data-show= "${show.idMeal}" ><i class="fa-sharp fa-regular fa-heart fa-xl"></i></button>
+  //               <div class="flex"><p class="like-${show.idMeal}" >5</p><p>like</p></div>
+  //               </div>
+  //           <button data-tap= "${show.idMeal}" class="btn btn-comment">Comment</button>
+  //       </div>
+  //       `;
+  //   })
+  //   .join('');
+  // mealContainer.insertAdjacentHTML('afterbegin', markUp);
 
   //   mealContainer.addEventListener('click', (e) => {
   //     e.preventDefault();
